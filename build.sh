f4f25211ebaeef9a2f710f61b1764a13f6d908f6
Anderson Clayton Pereira <anderson.cpereira@totvs.com.br>
2025-03-20 14:14:24 -0300

    Corrigir instalação do ChromeDriver no build.sh

--- build.sh
+++ build.sh
@@ -8,7 +8,10 @@ rm google-chrome-stable_current_amd64.deb
 # Instalar ChromeDriver
 wget https://storage.googleapis.com/chrome-for-testing-public/129.0.6668.100/linux64/chromedriver-linux64.zip
 unzip chromedriver-linux64.zip
+chmod +x chromedriver-linux64/chromedriver
 mv chromedriver-linux64/chromedriver /usr/local/bin/chromedriver
 rm -rf chromedriver-linux64.zip chromedriver-linux64
+# Verificar instalação
+/usr/local/bin/chromedriver --version
 # Instalar dependências Python
 pip install -r requirements.txt
