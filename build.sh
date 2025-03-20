a18ee989869745e6c629d6564a1e8a67d3bd8de1
Anderson Clayton Pereira <anderson.cpereira@totvs.com.br>
2025-03-20 13:34:53 -0300

    Corrigir build.sh e adicionar tenacity

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-apt-get update
-apt-get install -y wget unzip
+sudo apt-get update
+sudo apt-get install -y wget unzip
 # Instalar Google Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt-get install -y ./google-chrome-stable_current_amd64.deb
+sudo apt-get install -y ./google-chrome-stable_current_amd64.deb
 rm google-chrome-stable_current_amd64.deb
-# Instalar ChromeDriver
-wget https://chromedriver.storage.googleapis.com/129.0.6668.100/chromedriver_linux64.zip
-unzip chromedriver_linux64.zip -d /usr/local/bin/
-rm chromedriver_linux64.zip
+# Instalar ChromeDriver (versão mais recente disponível)
+wget https://storage.googleapis.com/chrome-for-testing-public/129.0.6668.100/linux64/chromedriver-linux64.zip
+unzip chromedriver-linux64.zip
+sudo mv chromedriver-linux64/chromedriver /usr/local/bin/chromedriver
+rm -rf chromedriver-linux64.zip chromedriver-linux64
 # Instalar dependências Python
 pip install -r requirements.txt
