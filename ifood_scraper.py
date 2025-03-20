167ddd7b2fc1ae5e684b70fe30eff1be45abb9e2
Anderson Clayton Pereira <anderson.cpereira@totvs.com.br>
2025-03-20 14:46:26 -0300

    Instalar chrome manualmente a ajustar caminho no ifood_scraper v3

--- ifood_scraper.py
+++ ifood_scraper.py
@@ -69,8 +69,11 @@ def configurar_driver(headless: bool = True) -> webdriver.Chrome:
     chrome_options = Options()
     if headless:
         chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--disable-gpu")  # Necessário em ambientes headless
+        chrome_options.add_argument("--disable-software-rasterizer")
+        chrome_options.add_argument("--remote-debugging-port=9222")  # Para evitar conflitos de porta
+    chrome_options.add_argument("--no-sandbox")  # Necessário no Render
+    chrome_options.add_argument("--disable-dev-shm-usage")  # Evita problemas de memória compartilhada
     chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36")
     chrome_options.add_argument("--ignore-certificate-errors")
     chrome_options.add_argument("--disable-web-security")
